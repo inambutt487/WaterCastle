e237152d3a56366a8938d68ffa50f5edcf316f78
MogheesIdrees <sheikh.moghees@innovotechnologies.com>
2025-06-23 15:47:05 +0500

    Initial Commit

---
+++ WaterCastle/WaterCastleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterCastleApp.swift
+//  WaterCastle
+//
+//  Created by Mac on 23/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct WaterCastleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
